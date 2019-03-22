12ad4339773092f3a9097caff98de17f8df200f8
Aerilius <aerilius@gmail.com>
2019-03-22 20:14:27 +0100

    Convert app to es6

---
+++ dialogs/components/app.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <vs-notify group="alert" position="top center" transition="ntf-top" />
+    <VsPrompt />
+    <TheJsonViewer />
+    <TheEntityPane ref="theEntityPane" 
+                     @selectedEntity="selectedEntity" 
+                     style="display: block; width: 100%; height: 2.5em; overflow: hidden;" />
+    <SplitPaneVertical :initial="30" 
+                         style="width: 100%; top: 2.5em; bottom: 0; position: absolute;">
+      <TheDictionaryPane slot="left" 
+                           ref="theDictionaryPane" 
+                           :selectedEntityId="selectedEntityId" 
+                           @selectedDictionary="selectedDictionary" 
+                           style="width: 100%; height: 100%; margin-top: 1px;" />
+      <TheAttributePane slot="right" 
+                          ref="theAttributePane" 
+                          :selectedDictionary="selectedPath" 
+                          style="width: 100%; height: 100%; margin-top: 1px;" />
+    </SplitPaneVertical>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import '../vendor/vs-notify.js'
+import VsPrompt from './vs-prompt.vue'
+import TheJsonViewer from './the-json-viewer.vue'
+import TheEntityPane from './the-entity-pane.vue'
+import SplitPaneVertical from './split-pane-vertical.vue'
+import TheDictionaryPane from './the-dictionary-pane.vue'
+import TheAttributePane from './the-attribute-pane.vue'
+
+// TODO: export default Vue.extend({
+// or    export default Vue.component({
+export default {
+  name: 'app',
+  components: {VsPrompt, VsPrompt, TheJsonViewer, TheEntityPane, SplitPaneVertical, TheDictionaryPane, TheAttributePane},
+  data () {
+    return {
+      selectedEntityId: null,
+      selectedPath: []
+    }
+  },
+  mounted () {
+    this.refresh()
+  },
+  methods: {
+    refresh () {
+      /*this.$refs.theEntityPane.refresh()*/
+    },
+    selectedEntity (entityId) {
+      console.log('app selectedEntity', entityId)
+      this.selectedEntityId = entityId
+      this.$refs.theDictionaryPane.refresh()
+    },
+    selectedDictionary (dictionaryPath) {
+      console.log('app selectedDictionary', dictionaryPath)
+      this.selectedPath = dictionaryPath
+      // Timeout so that changed property can be propagated into the-attribute-pane.
+      Vue.nextTick(this.$refs.theAttributePane.refresh)
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
