027666eb9f31ab88744183df8a044846013d73f2
Aerilius <aerilius@gmail.com>
2020-12-10 20:24:53 +0100

    Prefer adding a separator before your extension's menu entry when using any other menu than Extensions

--- src/ae_attribute_inspector/ui.rb
+++ src/ae_attribute_inspector/ui.rb
@@ -43,7 +43,10 @@ module AE
 
 
     def self.register_menu(command)
-      UI.menu('Window').add_item(command)
+      menu = UI.menu('Window')
+      # Prefer adding a separator before your extension's menu entry when using any other menu than Extensions.
+      menu.add_separator
+      menu.add_item(command)
     end
     private_class_method :register_menu
 
